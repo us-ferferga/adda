1e3c223d12058da36b92cf447435c56f19524e7f
Fernando Fernández <ferferga@hotmail.com>
2021-10-13 09:35:21 +0200

    PI1.2: Implementación iterativa

--- PI1/src/ejercicios/Ejercicio2.java
+++ PI1/src/ejercicios/Ejercicio2.java
@@ -1,5 +1,52 @@
 package ejercicios;
 
-public class Ejercicio2 {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+public class Ejercicio2 {
+	// Método provisto en el enunciado
+	 public static Map<Integer,List<String>> funcional (List<List<String>> listas) {
+		 return listas.stream()
+				 .flatMap(lista -> lista.stream())
+				 .collect(Collectors.groupingBy(String::length));
+	}
+	 
+	 public static Map<Integer,List<String>> recursiva (List<List<String>> listas) {
+		 return listas.stream()
+				 .flatMap(lista -> lista.stream())
+				 .collect(Collectors.groupingBy(String::length));
+	}
+	 
+	 public static Map<Integer,List<String>> iterativa (List<List<String>> listas) {
+		 Map<Integer, List<String>> result = new HashMap<Integer, List<String>>();
+		 Integer listSize = listas.size();
+		 Integer loop = 0;
+		 
+		 while (loop < listSize) {
+			 List<String> sublist = listas.get(loop);
+			 loop++;
+			 Integer subloop = 0;
+			 Integer subloopSize = sublist.size();
+			 
+			 while (subloop < subloopSize) {
+				 String str = sublist.get(subloop);
+				 subloop++;
+				 Integer strlen = str.length();
+				 
+				 if (result.containsKey(strlen)) {
+					 result.get(strlen).add(str);
+				 }
+				 else {
+					 List<String> list = new ArrayList<String>();
+					 list.add(str);
+					 result.put(strlen, list);
+				 }
+			 }
+		 }
+		 
+		 return result;
+	}
 }
--- PI1/src/tests/TestEjercicio2.java
+++ PI1/src/tests/TestEjercicio2.java
@@ -1,10 +1,71 @@
 package tests;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import ejercicios.Ejercicio2;
+import util.Helper;
+
 public class TestEjercicio2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void PrintWelcome() {
+		System.out.println("##################################################");
+		System.out.println("#                   Ejercicio 2                  #");
+		System.out.println("#         ficheros/PI1E2_DatosEntrada1.txt       #");
+		System.out.println("##################################################");
+		System.out.println("");
+	}
+	
+	public static void PrintWelcome2() {
+		System.out.println("##################################################");
+		System.out.println("#                   Ejercicio 2                  #");
+		System.out.println("#         ficheros/PI1E2_DatosEntrada2.txt       #");
+		System.out.println("##################################################");
+		System.out.println("");
 	}
 
+	public static void main(String[] args) throws IOException {
+		PrintWelcome();
+
+		List<String> lineasFichero1 = Helper.leerLineas(
+				System.getProperty("user.dir") + "\\src\\ficheros\\PI1E2_DatosEntrada1.txt"
+		);
+		List<String> lineasFichero2 = Helper.leerLineas(
+				System.getProperty("user.dir") + "\\src\\ficheros\\PI1E2_DatosEntrada2.txt"
+		);
+		
+		List<List<String>> elementosFichero1 = lineasFichero1
+				.stream()
+				.map(s -> Arrays.asList(s.split(",")))
+				.collect(Collectors.toList());
+		
+		List<List<String>> elementosFichero2 = lineasFichero2
+				.stream()
+				.map(s -> Arrays.asList(s.split(",")))
+				.collect(Collectors.toList());
+
+		System.out.println("Entrada: " + elementosFichero1);
+		System.out.println("1. Iterativa (while): " + 
+				Ejercicio2.iterativa(elementosFichero1));
+		System.out.println("2. Recursiva final: " + 
+				Ejercicio2.recursiva(elementosFichero1));
+		System.out.println("3. Funcional: " + 
+				Ejercicio2.funcional(elementosFichero1));
+		System.out.println("\n");
+		
+		PrintWelcome2();
+
+		System.out.println("Entrada: " + elementosFichero2);
+		System.out.println("1. Iterativa (while): " + 
+				Ejercicio2.iterativa(elementosFichero2));
+		System.out.println("2. Recursiva final: " + 
+				Ejercicio2.recursiva(elementosFichero2));
+		System.out.println("3. Funcional: " + 
+				Ejercicio2.funcional(elementosFichero2));
+		System.out.println("\n");
+	}
 }
