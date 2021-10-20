7d0d06484d847522ec3e4e1acd72d77f06bbbb7e
Fernando Fernández <ferferga@hotmail.com>
2021-10-20 13:22:37 +0200

    PI1.E4: Implementación iterativa y recursiva

--- PI1/src/ejercicios/Ejercicio4.java
+++ PI1/src/ejercicios/Ejercicio4.java
@@ -1,5 +1,66 @@
 package ejercicios;
 
-public class Ejercicio4 {
+import java.util.stream.Stream;
 
+public class Ejercicio4 {
+	private static Double cubo(Double n) {
+		return n * n * n;
+	}
+
+	private static Double error_newton(Double inicial, Double ultima) {
+		return Math.abs(inicial - ultima) / Math.abs(ultima);
+	}
+
+	private static void ComprobarVariables(Double error) {
+		if (!(error < 1 && error > 0)) {
+			throw new IllegalArgumentException("El error debe estar entre 0 y 1");
+		}
+	}
+
+	public static Double funcional(Double number, Double error) {
+		ComprobarVariables(error);
+		return Stream.iterate();
+	}
+
+	private static Double recursiva_interna(Double x, Double a, Double b, Double number, Double error) {
+		if (Math.abs(number - cubo(b)) > error) {
+
+			if (cubo(b) > number) {
+				a = b;
+			} else {
+				x = b;
+			}
+			return recursiva_interna(x, a, (x + a) / 2, number, error);
+		}
+		return x;
+	}
+
+	public static Double recursiva(Double number, Double error) {
+		ComprobarVariables(error);
+
+		Double x = 0.0;
+		Double a = number;
+		Double b = a / 2;
+
+		return recursiva_interna(x, a, b, number, error);
+	}
+
+	public static Double iterativa(Double number, Double error) {
+		ComprobarVariables(error);
+
+		Double x = 0.0;
+		Double a = number;
+		Double b = a / 2;
+
+		// Aplicamos método de Bisección
+		while (Math.abs(number - cubo(b)) > error) {
+			if (cubo(b) > number) {
+				a = b;
+			} else {
+				x = b;
+			}
+			b = (x + a) / 2;
+		}
+		return x;
+	}
 }
--- PI1/src/test/TestEjercicio4.java
+++ PI1/src/test/TestEjercicio4.java
@@ -1,10 +1,38 @@
 package test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import ejercicios.Ejercicio4;
+import util.Helper;
+
 public class TestEjercicio4 {
 
+	public static void PrintWelcome() {
+		System.out.println("##################################################");
+		System.out.println("#                   Ejercicio 4                  #");
+		System.out.println("#         ficheros/PI1E4_DatosEntrada.txt        #");
+		System.out.println("##################################################");
+		System.out.println("");
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Test");
+		PrintWelcome();
+
+		List<String> lineasFichero = Helper
+				.leerLineas(System.getProperty("user.dir") + "\\src\\ficheros\\PI1E4_DatosEntrada.txt");
+
+		for (String s : lineasFichero) {
+			List<String> splittedString = Arrays.asList(s.split(","));
+			Double number = Double.parseDouble(splittedString.get(0));
+			Double error = Double.parseDouble(splittedString.get(1));
+
+			System.out.println("Entrada: " + s);
+			System.out.println("1. Iterativa (while): " + Ejercicio4.iterativa(number, error));
+			System.out.println("2. Recursiva final: " + Ejercicio4.recursiva(number, error));
+			System.out.println("3. Funcional: " + Ejercicio4.funcional(number, error));
+			System.out.println("\n");
+		}
 	}
 
 }
