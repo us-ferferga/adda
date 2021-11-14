f858fe4c4d4b005a51cff25d42dee91c407beaaa
Fernando Fernández <ferferga@hotmail.com>
2021-11-14 18:09:52 +0100

    PI2.E3: Implementación

--- PI2/src/ejercicios/Ejercicio3.java
+++ PI2/src/ejercicios/Ejercicio3.java
@@ -1,7 +1,34 @@
 package ejercicios;
 
-public class Ejercicio3 {
+import java.util.List;
 
-	public static void recursiva() {
+import us.lsi.common.IntegerSet;
+
+public class Ejercicio3 {
+	
+	private static void recursiva_interna(List<Integer> rango, List<Integer> conjunto, Integer idx_inferior, Integer idx_superior, IntegerSet set) {
+		Integer inicio = rango.get(0);
+		Integer fin = rango.get(1);
+		
+		Integer inicio_conjunto = conjunto.get(idx_inferior);
+		Integer fin_conjunto = conjunto.get(idx_superior);
+		
+		if (inicio_conjunto >= inicio && inicio_conjunto < fin) {
+			set.add(inicio_conjunto);
+		}
+		
+		if (fin_conjunto < fin && fin_conjunto >= inicio) {
+			set.add(fin_conjunto);
+		}
+		
+		if (idx_superior != idx_inferior) {
+			recursiva_interna(rango, conjunto, idx_inferior + 1, idx_superior - 1, set);
+		}
+	}
+
+	public static IntegerSet recursiva(List<Integer> rango, List<Integer> conjunto) {
+		IntegerSet set = IntegerSet.empty();
+		recursiva_interna(rango, conjunto, 0, conjunto.size() - 1, set);
+		return set;
 	}
 }
--- PI2/src/test/TestEjercicio3.java
+++ PI2/src/test/TestEjercicio3.java
@@ -1,17 +1,59 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import ejercicios.Ejercicio3;
+import us.lsi.common.IntegerSet;
 import util.Helper;
 
 public class TestEjercicio3 {
 
+	private static List<Map<List<Integer>, List<Integer>>> procesado(List<String> lineasFichero) {
+		List<Map<List<Integer>, List<Integer>>> ls = new ArrayList<Map<List<Integer>, List<Integer>>>();
+		for (String s : lineasFichero) {
+			// Clave: Rango
+			// Valor: Conjunto
+			Map<List<Integer>, List<Integer>> mp = new HashMap<List<Integer>, List<Integer>>();
+			List<String> lista = Arrays.asList(s.split("#"));
+			List<String> str_num = Arrays.asList(lista.get(0).split(","));
+			List<String> str_rango = Arrays.asList(lista.get(1).split(","));
+
+			List<Integer> num = new ArrayList<Integer>();
+			List<Integer> rango = new ArrayList<Integer>();
+			for (String _num : str_num) {
+				num.add(Integer.valueOf(_num));
+			}
+
+			for (String _rango : str_rango) {
+				rango.add(Integer.valueOf(_rango));
+			}
+			mp.put(rango, num);
+			ls.add(mp);
+		}
+
+		return ls;
+	}
+
 	public static void main(String[] args) {
 		List<String> lineasFichero = Helper
 				.leerLineas(System.getProperty("user.dir") + "\\src\\ficheros\\PI2E3_DatosEntrada.txt");
+		List<Map<List<Integer>, List<Integer>>> conjuntos = procesado(lineasFichero);
 
-		for (String s : lineasFichero) {
+		for (Map<List<Integer>, List<Integer>> mp : conjuntos) {
+			List<Integer> rango = mp.keySet().stream().limit(1).toList().get(0);
+			List<Integer> conjunto = mp.get(rango);
+
+			IntegerSet resultado = Ejercicio3.recursiva(rango, conjunto);
+			System.out.println();
+			System.out.println("Lista: " + conjunto);
+			String rango_str = rango.toString();
+			rango_str = rango_str.substring(0, rango_str.length() - 1);
+			System.out.println("Rango: " + rango_str + ")");
+			System.out.println("Conjunto obtenido: " + resultado);
 		}
 	}
 }
