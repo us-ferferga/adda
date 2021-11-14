77efbc547f21f6eae6be3a0349083b5730d3eece
Fernando Fernández <ferferga@hotmail.com>
2021-11-14 18:28:48 +0100

    PI2.E4: Implementación

--- PI2/src/ejercicios/Ejercicio4.java
+++ PI2/src/ejercicios/Ejercicio4.java
@@ -1,13 +1,56 @@
 package ejercicios;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Ejercicio4 {
-	public static void recursiva_sin_memoria() {
+	public static Long recursiva_sin_memoria(Integer num) {
 		
+		if (num>2) {
+			return 2*recursiva_sin_memoria(num-1)+4*recursiva_sin_memoria(num-2)+6*recursiva_sin_memoria(num-3);
+		} else if (num == 2) {
+			return Integer.toUnsignedLong(6);
+		} else if (num == 1) {
+			return Integer.toUnsignedLong(4);
+		} else {
+			return Integer.toUnsignedLong(2);
+		}
 	}
-	public static void recursiva_con_memoria() {
+	
+	private static Long recursiva_interna(Integer num, Map<Integer, Long> mp) {
+		if (!mp.containsKey(num)) {
+			Long lng = 2*recursiva_interna(num-1, mp)+4*recursiva_interna(num-2, mp)+6*recursiva_interna(num-3, mp);
+			mp.put(num, lng);
+		}
 		
+		return mp.get(num);
 	}
-	public static void recursiva() {
+	
+	public static Long recursiva_con_memoria(Integer inicial) {
+		Map<Integer,Long> memoria = new HashMap<Integer, Long>();
 		
+		memoria.put(0, Integer.toUnsignedLong(2));
+		memoria.put(1, Integer.toUnsignedLong(4));
+		memoria.put(2, Integer.toUnsignedLong(6));
+		
+		return recursiva_interna(inicial, memoria);
+	}
+
+	public static Long iterativa(Integer n) {
+		List<Long> ls = new ArrayList<Long>();
+		ls.add(Integer.toUnsignedLong(2));
+		ls.add(Integer.toUnsignedLong(4));
+		ls.add(Integer.toUnsignedLong(6));
+		Integer i = 3;
+
+		while(i<=n) {
+			Long lng = (2*ls.get(i-1)+4*ls.get(i-2)+6*ls.get(i-3));
+			ls.add(lng);
+			i++;
+		}
+
+		return ls.get(n);
 	}
 }
--- PI2/src/test/TestEjercicio4.java
+++ PI2/src/test/TestEjercicio4.java
@@ -12,6 +12,14 @@ public class TestEjercicio4 {
 				.leerLineas(System.getProperty("user.dir") + "\\src\\ficheros\\PI2E4_DatosEntrada.txt");
 
 		for (String s : lineasFichero) {
+			Integer len = s.length();
+			Integer dato = Integer.valueOf(s.substring(2, len));
+			
+			System.out.println("Entero de entrada:     " + dato);
+			System.out.println("Sol. Rec. sin memoria: " + Ejercicio4.recursiva_sin_memoria(dato));
+			System.out.println("Sol. Rec. con memoria: " + Ejercicio4.recursiva_con_memoria(dato));
+			System.out.println("Sol. Iterativa:        " + Ejercicio4.iterativa(dato));
+			System.out.println("________________________________________________________________");
 		}
 	}
 
