e17482536b6ace682f5c162606f64c001e3e0585
Fernando Fernández <ferferga@hotmail.com>
2021-12-05 20:40:38 +0100

    PI3.E2: Implementación

--- PI3/src/ejercicios/Ejercicio2.java
+++ PI3/src/ejercicios/Ejercicio2.java
@@ -1,7 +1,33 @@
 package ejercicios;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import us.lsi.tiposrecursivos.BinaryTree;
+
 public class Ejercicio2 {
-	public static void recursiva() {
-		
+	public static Set<Integer> recursiva(BinaryTree<Integer> arbol, Integer num) {
+		return recursiva_interna(arbol, num, new HashSet<Integer>());
+	}
+	
+	private static Set<Integer> recursiva_interna(BinaryTree<Integer> arbol, Integer num, Set<Integer> set) {
+		Integer val = arbol.getLabel();
+		switch (arbol.getType()) {
+			case Leaf:
+				if (val >= num) {
+					set.add(val);
+				}
+				break;
+			case Binary:
+				if (val >= num) {
+					set.add(val);
+				}
+				set.addAll(recursiva_interna(arbol.getRight(), num, set));
+				set.addAll(recursiva_interna(arbol.getLeft(), num, set));
+				break;
+			default:
+				break;
+		}
+		return set;
 	}
 }
--- PI3/src/test/TestEjercicio2.java
+++ PI3/src/test/TestEjercicio2.java
@@ -1,17 +1,38 @@
 package test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import ejercicios.Ejercicio2;
+import us.lsi.common.Pair;
+import us.lsi.tiposrecursivos.BinaryTree;
 import util.Helper;
 
 public class TestEjercicio2 {
+	private static void imprimirSalida(BinaryTree<Integer> arbol, Set<Integer> resultado) {
+		String out = String.format("%s: %s", arbol, resultado);
+		System.out.println(out);
+	}
 
 	public static void main(String[] args) {
 		List<String> lineasFichero = Helper
 				.leerLineas(System.getProperty("user.dir") + "\\src\\ficheros\\PI3E2_DatosEntrada.txt");
+		
+		List<Pair<Integer, BinaryTree<Integer>>> datos = new ArrayList<Pair<Integer, BinaryTree<Integer>>>();
 
 		for (String s : lineasFichero) {
-			
+			var splitted = s.split("#");
+			Integer num = Integer.parseInt(splitted[1]);
+			BinaryTree<Integer> arbol = BinaryTree.parse(splitted[0], i -> Integer.parseInt(i));
+			datos.add(Pair.of(num, arbol));
+		}
+
+		System.out.println("SOLUCIÓN RECURSIVA:\n");
+		for (Pair<Integer, BinaryTree<Integer>> d : datos) {
+			Integer num = d.first();
+			BinaryTree<Integer> arbol = d.second();
+			imprimirSalida(arbol, Ejercicio2.recursiva(arbol, num));
 		}
 	}
 }
