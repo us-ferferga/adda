2bbf780f196453b7d7733dedc625745ba7a78840
Fernando Fernández <ferferga@hotmail.com>
2021-12-05 14:58:46 +0100

    PI3.E1: Resolución

--- PI3/src/ejercicios/Ejercicio1.java
+++ PI3/src/ejercicios/Ejercicio1.java
@@ -1,19 +1,41 @@
 package ejercicios;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import us.lsi.tiposrecursivos.Tree;
+
 public class Ejercicio1 {
-	public static void recursiva_par() {
-		
+	public static <E> Set<E> recursiva(Tree<E> arbol, Predicate<E> p) {
+		return recursiva_interna(arbol, p, new HashSet<E>());
 	}
 	
-	public static void recursiva_menor_que_cinco() {
-		
+	private static <E> Set<E> recursiva_interna(Tree<E> arbol, Predicate<E> p, Set<E> set) {
+		switch (arbol.getType()) {
+			case Leaf:
+				E val = arbol.getLabel();
+				if (p.test(val)) {
+					set.add(val);
+				}
+				break;
+			case Nary:
+				for (Tree<E> hijo : arbol.getChildren()) {
+					recursiva_interna(hijo, p, set);
+				}
+				break;
+			default:
+				break;
+		}
+		return set;
 	}
-	
-	public static void iterativa_par() {
-		
-	}
-	
-	public static void iterative_menor_que_cinco() {
-		
+
+	// Iterativa funcional
+	public static <E> Set<E> iterativa(Tree<E> arbol, Predicate<E> p) {
+		return arbol.stream()
+					.filter(a -> a.isLeaf() && p.test(a.getLabel()))
+					.map(a -> a.getLabel())
+					.collect(Collectors.toUnmodifiableSet());
 	}
 }
--- PI3/src/test/TestEjercicio1.java
+++ PI3/src/test/TestEjercicio1.java
@@ -1,17 +1,51 @@
 package test;
 
 import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+import ejercicios.Ejercicio1;
+import us.lsi.tiposrecursivos.Tree;
 import util.Helper;
 
 public class TestEjercicio1 {
+	private static void imprimirSalida(Tree<Integer> arbol, Set<Integer> resultado) {
+		String out = String.format("%s: %s", arbol, resultado);
+		System.out.println(out);
+	}
 
 	public static void main(String[] args) {
 		List<String> lineasFichero = Helper
 				.leerLineas(System.getProperty("user.dir") + "\\src\\ficheros\\PI3E1_DatosEntrada.txt");
+		
+		// Predicados del enunciado
+		Predicate<Integer> esPar = x -> x%2 == 0;
+		Predicate<Integer> menor5 = x-> x < 5;
+		
+		// Conversión a árboles
+		List<Tree<Integer>> arboles = lineasFichero.stream()
+													.map(s -> Tree.parse(s, i -> Integer.parseInt(i)))
+													.collect(Collectors.toUnmodifiableList());
 
-		for (String s : lineasFichero) {
-			
+		System.out.println("SOLUCIÓN RECURSIVA-PAR:\n");
+		for (Tree<Integer> a : arboles) {
+			imprimirSalida(a, Ejercicio1.recursiva(a, esPar));
+		}
+		
+		System.out.println("\nSOLUCIÓN RECURSIVA MENOR QUE CINCO:\n");
+		for (Tree<Integer> a : arboles) {
+			imprimirSalida(a, Ejercicio1.recursiva(a, menor5));
+		}
+		
+		System.out.println("\nSOLUCIÓN ITERATIVA PAR:\n");
+		for (Tree<Integer> a : arboles) {
+			imprimirSalida(a, Ejercicio1.iterativa(a, esPar));
+		}
+		
+		System.out.println("\nSOLUCIÓN ITERATIVA MENOR QUE CINCO:\n");
+		for (Tree<Integer> a : arboles) {
+			imprimirSalida(a, Ejercicio1.iterativa(a, menor5));
 		}
 	}
 }
