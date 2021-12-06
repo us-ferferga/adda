8e43c47cd78a235f72823e96fcb8e5986b6f3d5f
Fernando Fernández <ferferga@hotmail.com>
2021-12-06 22:24:22 +0100

    PI3.E5: Implementación

--- PI3/src/ejercicios/Ejercicio5.java
+++ PI3/src/ejercicios/Ejercicio5.java
@@ -1,11 +1,87 @@
 package ejercicios;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import us.lsi.tiposrecursivos.BinaryTree;
+
 public class Ejercicio5 {
-	public static void recursiva() {
+	private static Map<Paridad, List<Integer>> CrearMapa() {
+		Map<Paridad, List<Integer>> map = new HashMap<Paridad, List<Integer>>();
+		map.put(Paridad.Impar, new ArrayList<Integer>());
+		map.put(Paridad.Par, new ArrayList<Integer>());
 		
+		return map;
 	}
 	
-	public static void iterativa_funcional() {
+	private static void LimpiarMapa(Map<Paridad, List<Integer>> map) {
+		if (map.get(Paridad.Par).size() == 0) {
+			map.remove(Paridad.Par);
+		}
 		
+		if (map.get(Paridad.Impar).size() == 0) {
+			map.remove(Paridad.Impar);
+		}
+	}
+
+	public static Map<Paridad, List<Integer>> recursiva(BinaryTree<Integer> arbol) {
+		Map<Paridad, List<Integer>> map = CrearMapa();
+		recursiva_interna(arbol, map);
+		LimpiarMapa(map);
+
+		return map;
+	}
+	
+	private static Map<Paridad, List<Integer>> recursiva_interna(BinaryTree<Integer> arbol, Map<Paridad, List<Integer>> m) {
+		switch(arbol.getType()) {
+			case Binary:
+				Integer val = arbol.getLabel();
+				if (!arbol.getLeft().isEmpty() &&
+						!arbol.getRight().isEmpty() &&
+						val > arbol.getLeft().getLabel() &&
+						val < arbol.getRight().getLabel()
+					) {
+					if (val%2 == 0) {
+						m.get(Paridad.Par).add(val);
+					} else {
+						m.get(Paridad.Impar).add(val);
+					}
+				}
+				
+				recursiva_interna(arbol.getLeft(), m);
+				recursiva_interna(arbol.getRight(), m);
+				break;
+			default:
+				break;
+		}
+
+		return m;
+	}
+	
+	public static Map<Paridad, List<Integer>> iterativa_imperativa(BinaryTree<Integer> arbol) {
+		Map<Paridad, List<Integer>> map = CrearMapa();
+
+		for (BinaryTree<Integer> nodos: arbol) {
+			if (nodos.isBinary()) {
+				Integer val = nodos.getLabel();
+				if (!nodos.getLeft().isEmpty() &&
+						!nodos.getRight().isEmpty() &&
+						val > nodos.getLeft().getLabel() &&
+						val < nodos.getRight().getLabel()
+					) {
+					if (val%2 == 0) {
+						map.get(Paridad.Par).add(val);
+					} else {
+						map.get(Paridad.Impar).add(val);
+					}
+				}
+			}
+		}
+		
+		LimpiarMapa(map);
+		
+		return map;
 	}
 }
--- PI3/src/test/TestEjercicio5.java
+++ PI3/src/test/TestEjercicio5.java
@@ -1,16 +1,37 @@
 package test;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import ejercicios.Ejercicio5;
+import ejercicios.Paridad;
+import us.lsi.tiposrecursivos.BinaryTree;
 import util.Helper;
 
 public class TestEjercicio5 {
+	private static void imprimirSalida(BinaryTree<Integer> arbol, Map<Paridad, List<Integer>> resultado) {
+		String out = String.format("%s: %s", arbol, resultado);
+		System.out.println(out);
+	}
 
 	public static void main(String[] args) {
 		List<String> lineasFichero = Helper
 				.leerLineas(System.getProperty("user.dir") + "\\src\\ficheros\\PI3E5_DatosEntrada.txt");
+		
+		// Conversión a árboles
+		List<BinaryTree<Integer>> arboles = lineasFichero.stream()
+													.map(s -> BinaryTree.parse(s, i -> Integer.parseInt(i)))
+													.collect(Collectors.toUnmodifiableList());
 
-		for (String s : lineasFichero) {
+		System.out.println("SOLUCIÓN RECURSIVA:\n");
+		for (BinaryTree<Integer> arbol : arboles) {
+			imprimirSalida(arbol, Ejercicio5.recursiva(arbol));
+		}
+		
+		System.out.println("\nSOLUCIÓN ITERATIVA:\n");
+		for (BinaryTree<Integer> arbol : arboles) {
+			imprimirSalida(arbol, Ejercicio5.iterativa_imperativa(arbol));
 		}
 	}
 
