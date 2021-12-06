ed0dd554b2aa1498732a858aa8ec5f9bca100edf
Fernando Fernández <ferferga@hotmail.com>
2021-12-06 17:48:52 +0100

    PI3.E3: Implementación

--- PI3/src/ejercicios/Ejercicio3.java
+++ PI3/src/ejercicios/Ejercicio3.java
@@ -1,7 +1,41 @@
 package ejercicios;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import us.lsi.common.Pair;
+import us.lsi.tiposrecursivos.BinaryTree;
+
 public class Ejercicio3 {
-	public static void recursiva() {
+	public static Pair<List<Integer>, Integer> recursiva(BinaryTree<Integer> arbol) {
+		List<Integer> memoria = new ArrayList<Integer>();
 		
+		return recursiva_interna(arbol, Pair.of(null, null), memoria);
+	}
+	
+	private static Pair<List<Integer>, Integer> recursiva_interna(BinaryTree<Integer> arbol, Pair<List<Integer>, Integer> p, List<Integer> m) {
+		Integer val = null;
+		switch (arbol.getType()) {
+			case Leaf:
+				val = arbol.getLabel();
+				m.add(val);
+				Integer mult = m.stream().reduce(1, (a, b) -> a * b);
+				if (p.second() == null || mult > p.second()) {
+					p = Pair.of(m, mult);
+				}
+				break;
+			case Binary:
+				val = arbol.getLabel();
+				m.add(val);
+				List<Integer> copia_memoria = new ArrayList<>(m);
+				p = recursiva_interna(arbol.getLeft(), p, m);
+				// Para continuar con el camino más largo, es preciso que la segunda llamada recursiva utilice
+				// la copia de la lista original.
+				p = recursiva_interna(arbol.getRight(), p, copia_memoria);
+				break;
+			default:
+				break;
+		}
+		return p;
 	}
 }
--- PI3/src/test/TestEjercicio3.java
+++ PI3/src/test/TestEjercicio3.java
@@ -1,17 +1,32 @@
 package test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import ejercicios.Ejercicio3;
+import us.lsi.common.Pair;
+import us.lsi.tiposrecursivos.BinaryTree;
 import util.Helper;
 
 public class TestEjercicio3 {
 
+	private static void imprimirSalida(BinaryTree<Integer> arbol, Pair<List<Integer>, Integer> resultado) {
+		String out = String.format("%s: %s", arbol, resultado);
+		System.out.println(out);
+	}
+
 	public static void main(String[] args) {
 		List<String> lineasFichero = Helper
 				.leerLineas(System.getProperty("user.dir") + "\\src\\ficheros\\PI3E3_DatosEntrada.txt");
+		
+		// Conversión a árboles binarios
+				List<BinaryTree<Integer>> arboles = lineasFichero.stream()
+															.map(s -> BinaryTree.parse(s, i -> Integer.parseInt(i)))
+															.collect(Collectors.toUnmodifiableList());
 
-		for (String s : lineasFichero) {
-			
+		System.out.println("SOLUCIÓN RECURSIVA:\n");
+		for (BinaryTree<Integer> a : arboles) {
+			imprimirSalida(a, Ejercicio3.recursiva(a));
 		}
 	}
 }
