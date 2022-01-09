05906679903242fa7edd15511842af0c6326a362
Fernando Fernández <ferferga@hotmail.com>
2022-01-09 15:25:23 +0100

    PI4E2: Implementacion

--- PI4/src/ejercicios/Ejercicio2.java
+++ PI4/src/ejercicios/Ejercicio2.java
@@ -1,7 +1,82 @@
 package ejercicios;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Collections;
+
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.traverse.TopologicalOrderIterator;
+
+import us.lsi.colors.GraphColors;
+import us.lsi.colors.GraphColors.Color;
+import us.lsi.colors.GraphColors.Style;
+
 public class Ejercicio2 {
-	public static void graficar() {
+	public static List<String> a(SimpleDirectedGraph<String, DefaultEdge> graph, String salidaVistas) {
+		List<String> ls = graph.vertexSet().stream().toList();
+		List<String> books = new ArrayList<String>();
+
+		for (int i = 0; i < ls.size(); i++) {
+			if (graph.inDegreeOf(ls.get(i)) == 0) {
+				books.add(ls.get(i));
+			}
+		}
+
+		GraphColors.toDot(graph, salidaVistas, v -> v, e -> "",
+				v -> GraphColors.colorIf(Color.blue, Color.black, books.contains(v)),
+				a -> GraphColors.style(Style.solid));
+		System.out.println(String.format("APARTADO A): Dibujado el grafo en %s correctamente", salidaVistas));
+
+		return books;
 	}
-	
+
+	public static void b(SimpleDirectedGraph<String, DefaultEdge> graph, String salidaVistas, List<String> ls) {
+		DijkstraShortestPath<String, DefaultEdge> shtpath = new DijkstraShortestPath<String, DefaultEdge>(graph);
+		Boolean isValid = null;
+		GraphPath<String, DefaultEdge> gpath = null;
+		Set<String> vertexes = new HashSet<String>();
+		Set<DefaultEdge> edges = new HashSet<DefaultEdge>();
+
+		for (int i = 0; i < ls.size() - 1; i++) {
+			gpath = shtpath.getPath(ls.get(i), ls.get(i + 1));
+			if (gpath != null) {
+				vertexes.addAll(gpath.getVertexList());
+				edges.addAll(gpath.getEdgeList());
+				isValid = true;
+			} else {
+				isValid = false;
+			}
+		}
+
+		if (isValid) {
+			GraphColors.toDot(graph, salidaVistas, v -> v, e -> "",
+					v -> GraphColors.colorIf(Color.blue, Color.black, vertexes.contains(v)),
+					a -> GraphColors.colorIf(Color.blue, Color.black, edges.contains(a)));
+
+			System.out.println(String.format(
+					"APARTADO B): Para los libros %s hay solución.\nDibujado el grafo correspondiente en %s correctamente",
+					ls.toString(), salidaVistas));
+		} else {
+			System.out.println(String.format("APARTADO B): Para los libros %s no hay solución", ls.toString()));
+		}
+
+	}
+
+	public static void c(SimpleDirectedGraph<String, DefaultEdge> graph, String libro) {
+		DijkstraShortestPath<String, DefaultEdge> shtpath = new DijkstraShortestPath<String, DefaultEdge>(graph);
+		List<String> ls = graph.vertexSet().stream().filter(x -> !x.equals(libro) && shtpath.getPath(x, libro) != null)
+				.toList();
+
+		List<String> result = new ArrayList<String>();
+		TopologicalOrderIterator<String, DefaultEdge> topologicIterator = new TopologicalOrderIterator<String, DefaultEdge>(graph);
+		topologicIterator.forEachRemaining(v -> result.add(v));
+		graph.vertexSet().stream().filter(x -> !ls.contains(x)).forEach(x -> result.remove(x));
+		
+		System.out.println(String.format("Para %s previamente hay que leer %s", libro, result.toString()));
+	}
 }
--- PI4/src/test/TestEjercicio2.java
+++ PI4/src/test/TestEjercicio2.java
@@ -1,14 +1,68 @@
 package test;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
+
+import ejercicios.Ejercicio2;
 import util.Helper;
 
 public class TestEjercicio2 {
+	private static SimpleDirectedGraph<String, DefaultEdge> getGrafo(String path) {
+		List<String> lines = Helper.leerLineas(path);
+
+		SimpleDirectedGraph<String, DefaultEdge> graph = new SimpleDirectedGraph<>(String::new, DefaultEdge::new,
+				false);
+
+		for (String a : lines) {
+			if (a.contains("#")) {
+				continue;
+			}
+
+			String[] aristas = a.split(",");
+			
+			if (aristas.length > 1) {
+				graph.addEdge(aristas[0], aristas[1]);
+			} else if (!graph.containsVertex(aristas[0])) {
+				graph.addVertex(aristas[0]);
+			}
+		}
+
+		return graph;
+	}
+
+	private static void apartadoB(List<List<String>> lists, SimpleDirectedGraph<String, DefaultEdge> graph) {
+		for (int i = 0; i < lists.size(); i++) {
+			List<String> libros = lists.get(i);
+			String rutaSalida = String.format(System.getProperty("user.dir") + "\\src\\ficheros\\PI4E2_ApartadoB%d.gv",
+					i + 1);
+			new File(rutaSalida).delete();
+			Ejercicio2.b(graph, rutaSalida, libros);
+		}
+	}
+
 	public static void main(String[] args) {
-		List<String> lineasFichero = Helper
-				.leerLineas(System.getProperty("user.dir") + "\\src\\ficheros\\PI4E2_DatosEntrada1.txt");
-		List<String> lineasFichero2 = Helper
-				.leerLineas(System.getProperty("user.dir") + "\\src\\ficheros\\PI4E2_DatosEntrada2.txt");
+		SimpleDirectedGraph<String, DefaultEdge> graph = getGrafo(
+				System.getProperty("user.dir") + "\\src\\ficheros\\PI4E2_DatosEntrada1.txt");
+
+		System.out.println("-- EJERCICIO 2 --");
+		// Apartado A
+		String rutaSalidaA = System.getProperty("user.dir") + "\\src\\ficheros\\PI4E2_ApartadoA.gv";
+		new File(rutaSalidaA).delete();
+		Ejercicio2.a(graph, rutaSalidaA);
+
+		// Apartado B
+		List<List<String>> BTests = Stream.of(Arrays.asList("L1", "L6", "L8"), Arrays.asList("L1", "L8", "L3"),
+				Arrays.asList("L10", "L2", "L4", "L3", "L5")).toList();
+		apartadoB(BTests, graph);
+
+		// Apartado C
+		Ejercicio2.c(graph, "L3");
+		Ejercicio2.c(graph, "L9");
+		Ejercicio2.c(graph, "L7");
 	}
 }
