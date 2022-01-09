d4e39b474e72859905ab35110f56f29d97920c6c
Fernando Fernández <ferferga@hotmail.com>
2022-01-09 22:40:36 +0100

    PI4E4: Implementacion

--- PI4/src/ejercicios/Ejercicio4.java
+++ PI4/src/ejercicios/Ejercicio4.java
@@ -1,8 +1,71 @@
 package ejercicios;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import ejercicios.entidades.Distancia;
+import ejercicios.entidades.Persona;
+import us.lsi.colors.GraphColors;
+import us.lsi.colors.GraphColors.Color;
+import us.lsi.colors.GraphColors.Style;
+
 public class Ejercicio4 {
-	public static void graficar() {
+	static Predicate<Persona> tieneCovid = p -> p.getCovid();
+	
+	private static Boolean existeRiesgo(Collection<Persona> p) {
+		return p.stream().anyMatch(tieneCovid);
 	}
 	
-}
+	private static Color getColor(Persona persona, ConnectivityInspector<Persona, Distancia> connInspector) {
+		return persona.getCovid() ? Color.red
+				: existeRiesgo(connInspector.connectedSetOf(persona)) ? Color.orange
+						: Color.green;
+	}
+	
+	private static String getLabel(Persona persona, ConnectivityInspector<Persona, Distancia> connInspector,
+			DijkstraShortestPath<Persona, Distancia> shtpath) {
+		String result = persona.toString();
+		Stream<Persona> personasConCovid = connInspector.connectedSetOf(persona).stream().filter(tieneCovid);
+		Function<Persona, Integer> distanciaPersonas = p -> shtpath.getPath(persona, p).getLength();
 
+		if (getColor(persona, connInspector) == Color.orange) {
+			result += " -> " + Collections.min(personasConCovid.map(distanciaPersonas).toList()).toString();
+		} else if (getColor(persona, connInspector) == Color.red) {
+			result += " -> 0";
+		}
+
+		return result;
+	}
+	
+	public static Distancia pitagoras(Persona p1, Persona p2, Double distFilas, Double distColumnas) {
+		return Distancia.of(Math.sqrt(Math.pow((p1.getX() - p2.getX()) * distFilas, 2)
+				+ Math.pow((p1.getY() - p2.getY()) * distColumnas, 2)));
+	}
+
+	public static Integer AyB(SimpleWeightedGraph<Persona, Distancia> graph, String salidaVistas) {
+		ConnectivityInspector<Persona, Distancia> connInspector = new ConnectivityInspector<Persona, Distancia>(graph);
+		DijkstraShortestPath<Persona, Distancia> shtpath = new DijkstraShortestPath<Persona, Distancia>(graph);
+
+		GraphColors.toDot(graph, salidaVistas,v -> getLabel(v, connInspector, shtpath),
+				e -> String.format("%.3f", e.getDistance()),
+				v -> GraphColors.all(GraphColors.color(getColor(v, connInspector)), GraphColors.style(Style.filled)),
+				e -> GraphColors.color(Color.blue));
+
+		System.out.println(String.format("APARTADO A) y B): Dibujado el grafo en %s correctamente", salidaVistas));
+
+		List<Integer> listOfMins = graph.vertexSet().stream().filter(v -> getColor(v, connInspector) == Color.orange)
+				.map(v -> Collections.min(connInspector.connectedSetOf(v).stream().filter(tieneCovid)
+						.map(p -> shtpath.getPath(v, p).getLength()).toList()))
+				.toList();
+
+		return Collections.max(listOfMins);
+	}
+}
--- PI4/src/test/TestEjercicio4.java
+++ PI4/src/test/TestEjercicio4.java
@@ -1,15 +1,65 @@
 package test;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import ejercicios.Ejercicio4;
+import ejercicios.entidades.Distancia;
+import ejercicios.entidades.Persona;
+import us.lsi.common.Trio;
 import util.Helper;
 
 public class TestEjercicio4 {
-	public static void main(String[] args) {
-		List<String> lineasFichero = Helper
-				.leerLineas(System.getProperty("user.dir") + "\\src\\ficheros\\PI4E4_DatosEntrada1.txt");
-		List<String> lineasFichero2 = Helper
-				.leerLineas(System.getProperty("user.dir") + "\\src\\ficheros\\PI4E4_DatosEntrada1.txt");
+
+	public static Trio<Double, Double, List<Persona>> cargarDatos(String ruta) {
+		Double distFilas = .0;
+		Double distColumnas = .0;
+		List<Persona> personas = new ArrayList<>();
+
+		List<String> lineasFichero = Helper.leerLineas(ruta);
+		for (String linea : lineasFichero) {
+			if (linea.startsWith("distFilas=")) {
+				distFilas = Double.parseDouble(linea.replace("distFilas=", ""));
+			} else if (linea.startsWith("distColumnas=")) {
+				distColumnas = Double.parseDouble(linea.replace("distColumnas=", ""));
+			} else if (linea.startsWith("+")) {
+				personas.add(Persona.create(linea.replace("+", "").split(","), true));
+			} else if (linea.startsWith("-")) {
+				personas.add(Persona.create(linea.replace("-", "").split(","), false));
+			}
+		}
+		return Trio.of(distFilas, distColumnas, personas);
 	}
 
+	public static void main(String[] args) {
+		for (int i = 1; i < 3; i++) {
+			String path = System.getProperty("user.dir") + String.format("\\src\\ficheros\\PI4E4_DatosEntrada%d.txt", i);
+			Trio<Double, Double, List<Persona>> datos = cargarDatos(path);
+
+			SimpleWeightedGraph<Persona, Distancia> graph = new SimpleWeightedGraph<Persona, Distancia>(null, null);
+
+			List<Persona> copiaPersonas = new ArrayList<Persona>(datos.third());
+
+			for (Persona persona1 : datos.third()) {
+				graph.addVertex(persona1);
+				copiaPersonas.remove(copiaPersonas.indexOf(persona1));
+				for (Persona persona2 : copiaPersonas) {
+					graph.addVertex(persona2);
+
+					Distancia j = Ejercicio4.pitagoras(persona1, persona2, datos.first(), datos.second());
+					if (j.getDistance() < 1.2) {
+						graph.addEdge(persona1, persona2, j);
+					}
+				}
+			}
+			
+			String rutaSalida = System.getProperty("user.dir") + String.format("\\src\\ficheros\\PI4E4_ApartadoAyB_Test%d.gv", i);
+			new File(rutaSalida).delete();
+			Integer dias = Ejercicio4.AyB(graph, rutaSalida);
+			System.out.println(String.format("Para PI4E4_DatosEntrada%d.txt hacen falta %d días", i, dias));
+		}
+	}
 }
