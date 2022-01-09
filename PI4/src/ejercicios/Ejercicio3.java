6c1cd7a53b72f12625673e57aca044f95fcc36f3
Fernando Fernández <ferferga@hotmail.com>
2022-01-09 20:14:17 +0100

    PI4E3: Implementación

--- PI4/src/ejercicios/Ejercicio3.java
+++ PI4/src/ejercicios/Ejercicio3.java
@@ -1,7 +1,90 @@
 package ejercicios;
 
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
+import org.jgrapht.alg.shortestpath.FloydWarshallShortestPaths;
+import org.jgrapht.alg.tour.HeldKarpTSP;
+
+import ejercicios.entidades.Calle;
+import ejercicios.entidades.Interseccion;
+import us.lsi.colors.GraphColors;
+import us.lsi.colors.GraphColors.Color;
+import us.lsi.common.Pair;
+
 public class Ejercicio3 {
-	public static void graficar() {
+	public static void a(Graph<Interseccion, Calle> graph, String salidaVistas, Pair<String, String> pair) {
+		Interseccion i1 = null;
+		Interseccion i2 = null;
+
+		for (Interseccion i : graph.vertexSet()) {
+			if (i.getNombreMonumento().equals(pair.first())) {
+				i1 = i;
+			} else if (i.getNombreMonumento().equals(pair.second())) {
+				i2 = i;
+			} else if (i1 != null && i2 != null) {
+				break;
+			}
+		}
+
+		FloydWarshallShortestPaths<Interseccion, Calle> floydShtPath = new FloydWarshallShortestPaths<Interseccion, Calle>(
+				graph);
+		GraphPath<Interseccion, Calle> gp = floydShtPath.getPath(i1, i2);
+
+		GraphColors.toDot(graph, salidaVistas, Interseccion::toString, c -> c.getDuracion().toString(),
+				v -> GraphColors.colorIf(Color.blue, Color.black, gp.getVertexList().contains(v)),
+				a -> GraphColors.colorIf(Color.blue, Color.black, gp.getEdgeList().contains(a)));
+
+		System.out.println(String.format("APARTADO A): Dibujado el grafo en %s correctamente", salidaVistas));
 	}
-	
+
+	public static void b(Graph<Interseccion, Calle> graph, String salidaVistas) {
+		HeldKarpTSP<Interseccion, Calle> alg = new HeldKarpTSP<Interseccion, Calle>();
+		GraphPath<Interseccion, Calle> gp = alg.getTour(graph);
+
+		GraphColors.toDot(graph, salidaVistas, Interseccion::toString, a -> a.getEsfuerzo().toString(),
+				v -> GraphColors.colorIf(Color.blue, Color.black, gp.getVertexList().contains(v)),
+				a -> GraphColors.colorIf(Color.blue, Color.black, gp.getEdgeList().contains(a)));
+
+		System.out.println(String.format("APARTADO B): Dibujado el grafo en %s correctamente", salidaVistas));
+	}
+
+	public static void c(Graph<Interseccion, Calle> graph, String salidaVistas, List<Integer> callesCortadas) {
+		List<Calle> calles = new ArrayList<Calle>(graph.edgeSet());
+		for (Calle c : calles) {
+			if (callesCortadas.contains(c.getId())) {
+				graph.removeEdge(c);
+			}
+		}
+
+		ConnectivityInspector<Interseccion, Calle> connInspector = new ConnectivityInspector<Interseccion, Calle>(
+				graph);
+
+		List<Set<Interseccion>> ls = connInspector.connectedSets();
+		Set<Interseccion> monumentos = new HashSet<Interseccion>();
+
+		Integer total = 0;
+		for (int i = 0; i < ls.size(); i++) {
+			Integer sum = ls.get(i).stream().mapToInt(in -> in.getRelevancia()).sum();
+
+			if (sum >= total) {
+				total = sum;
+				monumentos = ls.get(i);
+			}
+		}
+
+		final Set<Interseccion> finalMonumentos = monumentos;
+		GraphColors.toDot(graph, salidaVistas, Interseccion::toString, c -> "",
+				v -> GraphColors.colorIf(Color.blue, Color.black, finalMonumentos.contains(v)),
+				a -> GraphColors.colorIf(Color.blue, Color.black, finalMonumentos.contains(graph.getEdgeSource(a))));
+
+		System.out.println(String.format("APARTADO C): Dibujado el grafo en %s correctamente quitando las calles %s",
+				salidaVistas, callesCortadas.toString()));
+	}
 }
--- PI4/src/test/TestEjercicio3.java
+++ PI4/src/test/TestEjercicio3.java
@@ -1,12 +1,45 @@
 package test;
 
-import java.util.List;
+import java.io.File;
+import java.util.Arrays;
 
-import util.Helper;
+import org.jgrapht.Graph;
+
+import ejercicios.Ejercicio3;
+import ejercicios.entidades.Calle;
+import ejercicios.entidades.Interseccion;
+import us.lsi.common.Pair;
+import us.lsi.graphs.Graphs2;
+import us.lsi.graphs.GraphsReader;
 
 public class TestEjercicio3 {
 	public static void main(String[] args) {
-		List<String> lineasFichero = Helper
-				.leerLineas(System.getProperty("user.dir") + "\\src\\ficheros\\PI4E3_DatosEntrada.txt");
+		String graphPath = System.getProperty("user.dir") + "\\src\\ficheros\\PI4E3_DatosEntrada.txt";
+		Graph<Interseccion, Calle> graphDuracion = GraphsReader.newGraph(graphPath, Interseccion::fromArray, Calle::fromArray,
+				Graphs2::simpleWeightedGraph, a -> Double.valueOf(a.getDuracion()));
+		Graph<Interseccion, Calle> graphEsfuerzo = GraphsReader.newGraph(graphPath, Interseccion::fromArray, Calle::fromArray,
+				Graphs2::simpleWeightedGraph, a -> Double.valueOf(a.getEsfuerzo()));
+
+		System.out.println("-- EJERCICIO 3 --");
+		// Apartado A
+		String rutaSalidaA1 = System.getProperty("user.dir") + "\\src\\ficheros\\PI4E3_ApartadoATest1.gv";
+		String rutaSalidaA2 = System.getProperty("user.dir") + "\\src\\ficheros\\PI4E3_ApartadoATest2.gv";
+		new File(rutaSalidaA1).delete();
+		new File(rutaSalidaA2).delete();
+		Ejercicio3.a(graphDuracion, rutaSalidaA1, Pair.of("m7", "m2"));
+		Ejercicio3.a(graphDuracion, rutaSalidaA2, Pair.of("m4", "m9"));
+
+		// Apartado B
+		String rutaSalidaB = System.getProperty("user.dir") + "\\src\\ficheros\\PI4E3_ApartadoB.gv";
+		new File(rutaSalidaB).delete();
+		Ejercicio3.b(graphEsfuerzo, rutaSalidaB);
+
+		// Apartado C
+		String rutaSalidaC1 = System.getProperty("user.dir") + "\\src\\ficheros\\PI4E3_ApartadoCTest1.gv";
+		String rutaSalidaC2 = System.getProperty("user.dir") + "\\src\\ficheros\\PI4E3_ApartadoCTest2.gv";
+		new File(rutaSalidaC1).delete();
+		new File(rutaSalidaC2).delete();
+		Ejercicio3.c(graphDuracion, rutaSalidaC1, Arrays.asList(0, 3, 5, 6));
+		Ejercicio3.c(graphDuracion, rutaSalidaC2, Arrays.asList(0, 3, 5, 6, 9));
 	}
 }
